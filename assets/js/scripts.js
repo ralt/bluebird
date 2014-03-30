438d27b1c49032371e424b9e74b7b1c1b6b2f8c4
Florian MARGAINE <florian@margaine.com>
2014-03-30 14:43:44 +0200

    Makes the JS code IE8 compatible

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,15 +4,15 @@
     var nav = document.createElement('div');
 
     var top = document.createElement('a');
-    top.textContent = '↑';
+    setText(top, '↑');
     top.href = '#top';
-    top.classList.add('top');
+    top.className = 'top';
 
     var link = document.createElement('a');
-    link.textContent = '⚓';
-    link.classList.add('anchor');
+    setText(link, '⚓');
+    link.className = 'anchor';
 
-    nav.classList.add('nav');
+    nav.className = 'nav';
     nav.appendChild(top);
     nav.appendChild(link);
 
@@ -22,11 +22,19 @@
     var h4s = document.querySelectorAll('h4');
     var h5s = document.querySelectorAll('h5');
 
-    [h1s, h2s, h3s, h4s, h5s].forEach(function(headings) {
-        [].forEach.call(headings, function(heading) {
-            var clone = nav.cloneNode(true);
+    var headings = [h1s, h2s, h3s, h4s, h5s];
+    var clone;
+    var heading;
+    for (var i = 0; i < headings.length; i++) {
+        for (var j = 0; j < headings[i].length; j++) {
+            heading = headings[i][j];
+            clone = nav.cloneNode(true);
             clone.children[1].href = '#' + heading.id;
             heading.parentNode.insertBefore(clone, heading);
-        });
-    });
+        }
+    }
+
+    function setText(el, text) {
+        el.textContent ? el.textContent = text : el.innerText = text;
+    }
 }());
